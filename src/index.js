76089ef538b53e70faa06829cee8405f37b50e9a
skarunamoorthy <skarunamoorthy@resqsoft.com>
2022-07-23 15:20:58 +0530

    updated done by siva

--- src/index.js
+++ src/index.js
@@ -800,7 +800,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:8081",
+                url: "https://sirius-latest.herokuapp.com/",
             },
         ],
         tags: [{
