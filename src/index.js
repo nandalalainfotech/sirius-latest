b86d76ae258bc354aa5116741dd335c89ca7fb24
skarunamoorthy <skarunamoorthy@resqsoft.com>
2022-07-28 15:44:46 +0530

    index.js changes done (refresh)

--- src/index.js
+++ src/index.js
@@ -64,7 +64,12 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 //     //   res.sendFile('index.html', { root: `${__dirname}/public/dist/omega` });
 //     res.sendFile(path.resolve(path.dirname('./src/public/dist/sirius')));
 // });
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, './src/public/dist/sirius')
+    );
+});
+app.use('*', (req, res, next) => {
     res.sendFile(
         path.join(__dirname, './src/public/dist/sirius')
     );
