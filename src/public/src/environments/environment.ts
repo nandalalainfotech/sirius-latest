2b8df86d12d6f900b1997327e8f441b0cb515eed
nandalalainfotech <nandalalainfotech@gmail.com>
2022-11-04 18:32:56 +0530

    Path changes done by Aravinth.

--- src/public/src/environments/environment.ts
+++ src/public/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  // baseUrl: 'https://siriusmatrimony.herokuapp.com/',
-  // apiUrl: 'https://siriusmatrimony.herokuapp.com/api'
-  baseUrl: 'http://127.0.0.1:8081',
-  apiUrl: 'http://localhost:8081/api'
+  baseUrl: 'https://siriusmatrimony.herokuapp.com/',
+  apiUrl: 'https://siriusmatrimony.herokuapp.com/api'
+  // baseUrl: 'http://127.0.0.1:8081',
+  // apiUrl: 'http://localhost:8081/api'
   
 };
